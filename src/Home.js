d71dbd785e5e0df5d7bff0032280063a9d5e8f93
Saikiran Sheshagiri <ssheshagiri@outlook.com>
2019-07-31 01:01:55 -0500

    Implemented Home screen

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
@@ -6,137 +6,202 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const Home = () => {
+import app from './firebase';
+import {AuthContext} from './Auth';
+import * as firebase from 'firebase';
 
-	const [show, setShow] = useState(false);
+class Home extends React.Component {
 
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
-
-	const [validated, setValidated] = useState(false);
-
-	const handleCreateMeeting = (event) => {
-		const form = event.currentTarget;
-		if (form.checkValidity() === false) {
-			event.preventDefault();
-			event.stopPropagation();
+	constructor(props) {
+		super(props);
+		this.state = {
+			show: false,
+			validated: false,
+			fields: {
+				title: "",
+				team: "",
+				accessCode: "",
+				meetingType: "planning"
+			},
+			recentMeetings: []
 		}
 
-		setValidated(true);
+		this.handleCreateMeeting = this.handleCreateMeeting.bind(this);
+		this.handleInputChange = this.handleInputChange.bind(this);
+	}
 
+	static contextType = AuthContext;
+
+	handleClose = () => {
+		this.setState({
+			show: false
+		})
+	};
+	handleShow = () => {
+		this.setState({
+			show: true
+		})
+	};
+
+	setValidated = (to) => {
+		this.setState({validated: to});
+	}
+
+	handleInputChange = (event) => {
+		const fields = this.state.fields;
+		fields[event.target.name] = event.target.value;
+		this.setState({fields});
+	}
+
+	handleCreateMeeting = event => {
+		
+		this.setValidated(true);
+
+		if (this.state.fields.title === "" || this.state.fields.team === "" || this.state.fields.accessCode === "") {
+			return;
+		} else {
+			const {currentUser} = this.context;
+			const db = app.firestore();
+			db.collection("planning").add({
+				title: this.state.fields.title,
+				team: this.state.fields.team,
+				accessCode: this.state.fields.accessCode,
+				hostId: currentUser.uid,
+				meetingType: this.state.fields.meetingType,
+				createDate: firebase.firestore.Timestamp.now()
+			})
+			.then((documentRef) => {
+				this.setState({
+					show: false
+				});
+			})
+			.catch((error) => {
+				alert("error creating meeting information:" + error);
+			});
+		}
 
 	}
 
-	return (
-		<div className="container home">
-			<Button variant="success" onClick={handleShow}>Start a new meeting</Button>
-			<hr/>
-			<h4>Recent meetings</h4>
-			<table className="table">
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th>Team</th>
-						<th>Meeting Type</th>
-						<th>Date</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>Sprint 42</td>
-						<td>SkyNet</td>
-						<td>Planning</td>
-						<td>June 30, 2019</td>
-					</tr>
-					<tr>
-						<td>Sprint 41</td>
-						<td>Insco</td>
-						<td>Planning</td>
-						<td>June 12, 2019</td>
-					</tr>
-					<tr>
-						<td>Sprint 41</td>
-						<td>SkyNet</td>
-						<td>Retro</td>
-						<td>May 29, 2019</td>
-					</tr>
-					<tr>
-						<td>Sprint 40</td>
-						<td>Incredibles</td>
-						<td>Planning</td>
-						<td>May 28, 2019</td>
-					</tr>
-					<tr>
-						<td>Sprint 39</td>
-						<td>Avengers</td>
-						<td>Retro</td>
-						<td>May 12, 2019</td>
-					</tr>
-				</tbody>
-			</table>
-			<Modal size="lg" show={show} onHide={handleClose}>
-				<Modal.Header closeButton>
-				<Modal.Title>Create a new meeting</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					<Form noValidate validated={validated} onSubmit={handleCreateMeeting}>
-						<Form.Group as={Row} controlId="validateMeetingName">
-							<Form.Label column sm="3">Title</Form.Label>
-							<Col sm="9">
-								<Form.Control
-									required
-									type="text"
-									placeholder="Enter meeting name"
-									defaultValue=""
-								></Form.Control>
-							</Col>
-						</Form.Group>
-						<Form.Group as={Row}>
-							<Form.Label column sm="3">Team</Form.Label>
-							<Col sm="9">
-								<Form.Control
-									required
-									type="text"
-									placeholder="Enter Team name"
-								></Form.Control>
-							</Col>
-						</Form.Group>
-						<Form.Group as={Row}>
-							<Form.Label column sm="3">
-								Access Code
-							</Form.Label>
-							<Col sm="9">
-								<Form.Control
-									required
-									type="text"
-									placeholder="Enter an access code"
-								></Form.Control>
-							</Col>
-						</Form.Group>
-						<Form.Group as={Row}>
-							<Form.Label column sm="3">
-								Meeting Type
-							</Form.Label>
-							<Col sm="9">
-								<Form.Control as="select">
-									<option>Planning Poker</option>
-									<option>Retro meeting</option>
-								</Form.Control>
-							</Col>
-						</Form.Group>
-					</Form>
-				</Modal.Body>
-				<Modal.Footer>
-					<Button variant="secondary" onClick={handleClose}>
-						Close
-					</Button>
-					<Button variant="primary" onClick={handleCreateMeeting}>
-						Submit
-					</Button>
-				</Modal.Footer>
-			</Modal>
-		</div>
-	)
+	componentDidMount() {
+		const {currentUser} = this.context;
+		const db = app.firestore();
+		db.collection("planning").where("hostId", "==", currentUser.uid)
+			.onSnapshot((querySnapshot) => {
+				let recentMeetings = [];
+				querySnapshot.forEach(function(doc) {
+					recentMeetings.push(doc);
+				})
+				this.setState({recentMeetings});
+			});
+	}
+
+	render() {
+		return (
+			<div className="container home">
+				<Button variant="success" onClick={this.handleShow}>Start a new meeting</Button>
+				<hr/>
+				<h4>Recent meetings</h4>
+				<table className="table">
+					<thead>
+						<tr>
+							<th>Title</th>
+							<th>Team</th>
+							<th>Meeting Type</th>
+							<th>Date</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							this.state.recentMeetings.map((meeting) => {
+
+								let docData = meeting.data();
+								console.log(docData);
+								return(<tr key={meeting.id}>
+									<td>{docData.title}</td>
+									<td>{docData.team}</td>
+									<td>{docData.meetingType}</td>
+									<td>{docData.createDate.toDate().toDateString()} {docData.createDate.toDate().toLocaleTimeString()}</td>
+								</tr>)
+							})
+						}
+						
+					</tbody>
+				</table>
+				<Modal size="lg" show={this.state.show} onHide={this.handleClose}>
+					<Modal.Header closeButton>
+					<Modal.Title>Create a new meeting</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<Form noValidate validated={this.state.validated} onSubmit={this.handleCreateMeeting}>
+							<Form.Group as={Row} controlId="validateMeetingName">
+								<Form.Label column sm="3">Title</Form.Label>
+								<Col sm="9">
+									<Form.Control
+										required
+										type="text"
+										name="title"
+										placeholder="Enter meeting name"
+										value={this.state.fields.title}
+										onChange={this.handleInputChange}
+									></Form.Control>
+								</Col>
+							</Form.Group>
+							<Form.Group as={Row}>
+								<Form.Label column sm="3">Team</Form.Label>
+								<Col sm="9">
+									<Form.Control
+										required
+										type="text"
+										name="team"
+										placeholder="Enter Team name"
+										value={this.state.fields.team}
+										onChange={this.handleInputChange}
+									></Form.Control>
+								</Col>
+							</Form.Group>
+							<Form.Group as={Row}>
+								<Form.Label column sm="3">
+									Access Code
+								</Form.Label>
+								<Col sm="9">
+									<Form.Control
+										required
+										type="text"
+										name="accessCode"
+										placeholder="Enter an access code"
+										value={this.state.fields.accessCode}
+										onChange={this.handleInputChange}
+									></Form.Control>
+								</Col>
+							</Form.Group>
+							<Form.Group as={Row}>
+								<Form.Label column sm="3">
+									Meeting Type
+								</Form.Label>
+								<Col sm="9">
+									<Form.Control as="select" 
+											name="meetingType"
+											value={this.state.fields.meetingType} 
+											onChange={this.handleInputChange}>
+										<option value="planning">Planning Poker</option>
+										<option value="retro">Retro meeting</option>
+									</Form.Control>
+								</Col>
+							</Form.Group>
+						</Form>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button variant="secondary" onClick={this.handleClose}>
+							Close
+						</Button>
+						<Button variant="primary" onClick={this.handleCreateMeeting}>
+							Submit
+						</Button>
+					</Modal.Footer>
+				</Modal>
+			</div>
+		)
+	}
 }
 
 
